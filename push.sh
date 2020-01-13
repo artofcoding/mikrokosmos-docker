a439642fc4541de2352162590cfa74db6fbffdab
Ralf Bensmann <ralf@art-of-coding.eu>
2020-01-13 20:25:39 +0100

    Push a release by tagging a commit

---
+++ push.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+
+if [ $# -eq 0 ]
+then
+    echo "usage: $0 <tag>"
+    exit 1
+fi
+
+TAG=$1
+
+git push origin :refs/tags/"${TAG}"
+git tag -f "${TAG}"
+git push origin
+
+exit 0
