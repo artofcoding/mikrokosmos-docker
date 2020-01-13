2ec51b99937a3c934d3a2819b5b9f8d8c3e3c75e
Ralf Bensmann <ralf@art-of-coding.eu>
2020-01-13 19:59:36 +0100

    Setup for ArchLinux

---
+++ bin/setup-archlinux.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019 art of coding UG, https://www.art-of-coding.eu
+# Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+# All rights reserved. Use is subject to license terms.
+#
+
+set -o nounset
+set -o errexit
+
+if [[ "$(id -un)" != "root" ]]
+then
+    echo "Please execute as root"
+    exit 1
+fi
+
+#
+# Time
+#
+
+cp /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+echo "Europe/Berlin" >/etc/timezone
+#echo "LANG=de_DE.UTF-8" >/etc/locale.conf
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
+
+#
+# Linux Packages
+#
+
+pacman --noconfirm -Syyu
+
+pacman --noconfirm -S sudo
+#archlinux_install linux-lts
+#grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm -S git
+
+#
+# SSH
+#
+
+pkfile=$HOME/.ssh/id_rsa
+if [[ ! -f ${pkfile} ]]
+then
+    ssh-keygen -t rsa -b 4096 -N "" -f ${pkfile}
+fi
+ssh-keygen -R github.com
+ssh-keyscan github.com >>$HOME/.ssh/known_hosts
+ssh-keygen -R bitbucket.org
+ssh-keyscan bitbucket.org >>$HOME/.ssh/known_hosts
+
+#
+# Docker
+#
+
+# Install Docker
+pacman --noconfirm -S docker docker-compose
+# Docker Logfiles
+logrotate_docker=/etc/logrotate.d/docker
+cat >${logrotate_docker} <<EOF
+/var/lib/docker/containers/*/*.log {
+        rotate 30
+        daily
+        compress
+        missingok
+        delaycompress
+        copytruncate
+}
+EOF
+systemctl enable docker
+systemctl start docker
+
+#
+# ZSH
+#
+
+pacman --noconfirm -S zsh
+usermod -s /bin/zsh root
+
+exit 0
